243db641bec14498d071537e313c3d054210c929
Razig17 <abdelrazig.sharif1@gmail.com>
2024-10-27 20:26:23 +0300

    Created store page to list products

--- store/urls.py
+++ store/urls.py
@@ -2,4 +2,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
+    path("store/", views.store, name="store"),
+    path("store/smartphones", views.smartphones, name="smartphones"),
+    path("store/laptops", views.laptops, name="laptops"),
+    path("store/<int:id>", views.product, name="product"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
+from .models import Product
+
 
 def index(request):
     return render(request, "index.html" , {})
+
+
+def store(request):
+    query = request.GET.get('query', (""))
+    category = request.GET.get('category', 0)
+    if category == "0":
+        products = Product.objects.filter(name__contains=query)
+    elif category != 0:
+        products = Product.objects.filter(category__name=category, name__contains=query)
+    else:
+        products = Product.objects.all()
+    return render(request, "store.html" , {"products": products})
+
+
+def smartphones(request):
+    products = Product.objects.filter(category__name="smartphones")
+    return render(request, "smartphones.html" , {"products": products})
+
+
+def laptops(request):
+    products = Product.objects.filter(category__name="laptops")
+    return render(request, "laptops.html" , {"products": products})
+
+
+def product(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, "product.html" , {"product": product})
